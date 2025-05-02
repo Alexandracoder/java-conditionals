dddb4e70cfea5bdab2ef907fdc45e47c8fcac435
Alexandracoder <acasaviajes@gmail.com>
2025-05-03 01:08:25 +0200

    Weather done

--- Weather.java
+++ Weather.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Weather {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa la temperatura de tu ciudad en grados Celcius: ");
+        int temperatura = scanner.nextInt();
+        if (temperatura <= 0) {
+            System.out.println("Te estás congelando");
+        }
+    else if (temperatura >= 0 && temperatura <= 10) {
+        System.out.println("Hace mucho frío");
+        }
+    else if (temperatura >= 11 && temperatura <= 17) {
+    System.out.println("Con un abrigo estás bien");
+    }
+        else if (temperatura >= 18 && temperatura <= 25) {
+            System.out.println("Parece que ha llegado el verano");
+        }
+        else if (temperatura >=26 && temperatura <=35) {
+            System.out.println("Que calooorrrrr");
+        }
+        else if (temperatura >=36) {
+            System.out.println("Ahí no hay quien viva");
+        }
+        scanner.close();
+
+
+
         //Escribe un programa que pida al usuario que ingrese la temperatura en celcius de su ciudad (La pregunta y la respuesta deberá estar en una sola línea) y según ello devuelva lo siguiente:
         //Si es bajo 0: Te estás congelando
         //Si es entre 0 y 10: Hace mucho frío 
