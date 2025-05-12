68d8b39b4b0df1cbcd413db3a0a75e4faaddb932
Alexandracoder <acasaviajes@gmail.com>
2025-05-12 23:53:02 +0200

    Fixing file

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class TrafficLights {
     public static void main(String[] args) {
-        // Variable de entrada
-        String colorSemaforo = "verde"; // Puedes cambiarlo a "amarillo", "rojo", u otro valor
+        String colorSemaforo = "verde";
         final String MENSAJE_VERDE = "Puedes seguir";
         final String MENSAJE_AMARILLO = "Ten cuidado";
         final String MENSAJE_ROJO = "¡¡¡Para!!!";
@@ -10,26 +9,26 @@ public class TrafficLights {
 
         switch (colorSemaforo.toLowerCase()) {
             case "verde":
-                System.out.println("Puedes seguir");
+                System.out.println(MENSAJE_VERDE);
                 break;
             case "amarillo":
-                System.out.println("Ten cuidado");
+                System.out.println(MENSAJE_AMARILLO);
                 break;
             case "rojo":
-                System.out.println("¡¡¡Para!!!");
+                System.out.println(MENSAJE_ROJO);
                 break;
             default:
-                System.out.println("El semáforo está dañado");
+                System.out.println(MENSAJE_OTRO);
                 break;
         }
     }
 }
         // trying and learning final to save memory
-        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
-        
+        //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor
+
         //Si es verde imprime en la terminal: Puedes seguir
         //Si es amarillo imprime en la terminal: Ten cuidado
         //Si es rojo imprime en la terminal: ¡¡¡Para!!!
         //Si es otro color imprime en la terminal: El semáforo está dañado
 
-        //Cambia el valor de la variable, corre el programa y mira el resultado 
+        //Cambia el valor de la variable, corre el programa y mira el resultado
