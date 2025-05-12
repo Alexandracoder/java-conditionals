f7e5c934d97fc76a9b49de41bf92d4e72410fa4a
Alexandracoder <acasaviajes@gmail.com>
2025-05-12 22:48:24 +0200

    Adding Quizz and TrafficLights

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
-
 public class TrafficLights {
     public static void main(String[] args) {
-        final String color = "gris";
-        if ("verde".equals(color)) {
-            System.out.println("Puedes seguir");
-        } else if ("amarillo".equals(color)) {
-            System.out.println("Ten cuidado");
-        } else if ("rojo".equals(color)) {
-            System.out.println("¡¡¡Para!!!");
-        } else {
-            System.out.println("El semáforo está dañado");
+        // Variable de entrada
+        String colorSemaforo = "verde"; // Puedes cambiarlo a "amarillo", "rojo", u otro valor
+        final String MENSAJE_VERDE = "Puedes seguir";
+        final String MENSAJE_AMARILLO = "Ten cuidado";
+        final String MENSAJE_ROJO = "¡¡¡Para!!!";
+        final String MENSAJE_OTRO = "El semáforo está dañado";
+
+        switch (colorSemaforo.toLowerCase()) {
+            case "verde":
+                System.out.println("Puedes seguir");
+                break;
+            case "amarillo":
+                System.out.println("Ten cuidado");
+                break;
+            case "rojo":
+                System.out.println("¡¡¡Para!!!");
+                break;
+            default:
+                System.out.println("El semáforo está dañado");
+                break;
         }
     }
-    }
+}
         // trying and learning final to save memory
         //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
         
